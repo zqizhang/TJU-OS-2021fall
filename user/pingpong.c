b83eb02e11ae0f8c5a4a501839d6ec9a5fb4e46f
WangXun <zcsryu2@126.com>
2021-10-18 22:33:19 +0800

    test

---
+++ user/pingpong.c
@@ -0,0 +1,8 @@
+//pingpong fuction
+#include "kernel/types.h"
+#include "user/user.h"
+// #include "kernel/pipe.c"
+
+void main(int argc, char* argv){
+    
+}
---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+//sleep fuction
+#include "kernel/types.h"
+#include "user/user.h"
+
+int atoi(const char *s);
+
+
+void main(int argc, char *argv[]){
+    if(argc == 1)
+        printf("Error! Please input the number of ticks.\n");
+    else if(argc == 2){
+        int num;
+        num = atoi(argv[1]);
+        sleep(num);
+    }
+    else{
+        printf("Error! You input too much parameter or no parameter.\n");
+    }
+    exit(0);
+}
