014ff4c1914605c89459b8db021dcbe923caf379
WangXun <zcsryu2@126.com>
2021-10-19 23:25:21 +0800

    sleep pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,5 +4,23 @@
 // #include "kernel/pipe.c"
 
 void main(int argc, char* argv){
-    
+    int fd_pa[2], fd_ch[2];//written by parent and child
+    char buf[5];
+    if(pipe(fd_pa) == -1 || pipe(fd_ch) == -1){
+        printf("Fail to create pipe\n");
+    }
+    if(fork() > 0){
+        write(fd_pa[1], "ping", strlen("ping"));
+        read(fd_ch[0], buf, strlen("pong"));
+        printf("%d: received %s\n", getpid(), buf);
+    }
+    else if(fork() == 0){
+        read(fd_pa[0], buf, strlen("ping"));
+        printf("%d: received %s\n", getpid(), buf);
+        write(fd_ch[1], "pong", strlen("ping"));
+    }
+    else if(fork() == -1){
+        printf("Fail to create process\n");
+    }
+    exit(0);
 }
