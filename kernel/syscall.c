7303a135bad09636da35335f41421e16d390b761
WangXun <zcsryu2@126.com>
2021-10-26 15:50:30 +0800

    trace

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -134,6 +134,31 @@ static uint64 (*syscalls[])(void) = {
 [SYS_trace]   sys_trace,
 };
 
+static char* sysname[] = {
+  [SYS_fork]    "fork",
+  [SYS_exit]    "exit",
+  [SYS_wait]    "wait",
+  [SYS_pipe]    "pipe",
+  [SYS_read]    "read",
+  [SYS_kill]    "kill",
+  [SYS_exec]    "exec",
+  [SYS_fstat]   "fstat",
+  [SYS_chdir]   "chdir",
+  [SYS_dup]     "dup",
+  [SYS_getpid]  "getpid",
+  [SYS_sbrk]    "sbrk",
+  [SYS_sleep]   "sleep",
+  [SYS_uptime]  "uptime",
+  [SYS_open]    "open",
+  [SYS_write]   "write",
+  [SYS_mknod]   "mknod",
+  [SYS_unlink]  "unlink",
+  [SYS_link]    "link",
+  [SYS_mkdir]   "mkdir",
+  [SYS_close]   "close",
+  [SYS_trace]   "trace",
+};
+
 void
 syscall(void)
 {
@@ -143,6 +168,10 @@ syscall(void)
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
+
+    if((1 << num) & p->trace_mask){
+      printf("%d: syscall %s -> %d\n", p->pid, sysname[num], p->trapframe->a0);
+    }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -97,6 +97,12 @@ sys_uptime(void)
 }
 
 uint64 
-sys_trace(){
-  
+sys_trace(void){
+  int mask;
+
+  if(argint(0, &mask) < 0)
+    return -1;
+
+  myproc()->trace_mask = mask;
+  return 0;
 }
