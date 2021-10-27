efef19a534e5ee3ec8d2e43fed73b43caeec851f
WangXun <zcsryu2@126.com>
2021-10-27 10:45:45 +0800

    finished trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -282,6 +282,7 @@ fork(void)
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
+  
   np->trace_mask = p->trace_mask;
 
   // increment reference counts on open file descriptors.
