2d161979054902aea66191eee01439ab2d4e49cc
WangXun <zcsryu2@126.com>
2021-10-27 10:44:36 +0800

    finished trace

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,5 +104,5 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   
-  int trace_mask;              // 
+  int trace_mask;              // the mask of system calls
 };
